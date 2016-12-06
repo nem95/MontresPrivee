ec9a468d77903196b8591e90f23fed0f409451ed
naim <naiim.chatti@gmail.com>
2016-12-06 15:44:21 +0100

    Modif home page

---
+++ montrePrive/www/js/HomeController.js
@@ -0,0 +1,33 @@
+angular.module("HomeController", [])
+
+  .controller('mainController', function($scope) {
+
+      //on declare le scope slide, qui pourra etre utilisé dans le slider dans la vue home.html
+      $scope.slide = [
+        "http://lovetime.fr/wp-content/uploads/2012/11/breitling-transocean-chronograph-unitime-1.jpg",
+        "http://img1.gtsstatic.com/montre/la-montre-grandmaster-chime-ref-5175-de-patek-philippe_160321_w460.jpg",
+        "http://www.masculin.com/images/article/11407/th/montre-breitling-bentley-b06-s-800x.jpg",
+        "http://image.excite.fr/hommes/guide/montres-homme-de-luxe-default-33931-0.jpg",
+      ];
+
+      $scope.options = {
+        effect: 'fade',
+        autoplay: 2000,
+        speed: 500,
+      }
+
+      $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+        // data.slider is the instance of Swiper
+        $scope.slider = data.slider;
+      });
+
+      $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+        console.log('Slide change is beginning');
+      });
+
+      $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+        // note: the indexes are 0-based
+        $scope.activeIndex = data.slider.activeIndex;
+        $scope.previousIndex = data.slider.previousIndex;
+      });
+  })
